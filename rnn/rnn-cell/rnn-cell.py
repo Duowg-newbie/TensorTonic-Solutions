e59e6e57f58987b6eab3e5acc5622c071f3783a1
Duowg-newbie <daoduytungduong@gmail.com>
2026-05-18 01:15:00 +0700

    Update solution for rnn-cell

--- rnn/rnn-cell/rnn-cell.py
+++ rnn/rnn-cell/rnn-cell.py
@@ -5,9 +5,9 @@ def rnn_cell(x_t: np.ndarray, h_prev: np.ndarray,
     """
     Single RNN cell forward pass.
     """
-    # YOUR CODE HERE
-    input_part = np.dot(h_prev, W_hh.T)
-    hidden_part = np.dot(x_t, W_xh.T)
+    # YOUR CODE HEREnp.dot(x_t, W_xh.T)
+    input_part = np.dot(x_t, W_xh.T)
+    hidden_part = np.dot(h_prev, W_hh.T)
     linear_combination = input_part + hidden_part + b_h
     h_t = np.tanh(linear_combination)
     return h_t
